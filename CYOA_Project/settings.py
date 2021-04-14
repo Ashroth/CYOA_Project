f05bd2ed6a596a90845b2832f029883db548bfba
Ashroth <jaakko.maalampi@gmail.com>
2021-04-14 19:12:02 +0300

    Corrected something

--- CYOA_Project/settings.py
+++ CYOA_Project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!k#9%08y$wr9%s9v7$dxw%0powg816&2r(o(86y)ex0l6hsy*v'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
