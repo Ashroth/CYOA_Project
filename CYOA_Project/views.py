d3dafbe6c64ea2caab66cacff3e3d9e71e7a770c
Ashroth <jaakko.maalampi@gmail.com>
2021-04-19 21:19:57 +0300

    Basic register code

---
+++ CYOA_Project/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.db import IntegrityError
+from django.shortcuts import render
+from django.urls import reverse
+from django import forms
+
+def login_view(request):
+    pass
+
+def register_view(request):
+    if request.method == "GET":
+        return render("register.html")
+    if request.method == "POST":
+        user = request.POST["user"]
+        password = request.POST["password"]
+        confirm = request.POST["confirm"]
+        email = request.POST["email"]
+        if password != confirm:
+            pass
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            pass
+        login(request.user)
+        return HttpResponseRedirect(reverse('index'))
